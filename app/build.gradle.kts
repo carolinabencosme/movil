190b3b2b23276909606a6c542f73df0c98b38d78
Carolina <cbencome@gmail.com>
2025-09-25 01:26:20 -0400

    Replace Material Design library with Compose Material3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     androidTestImplementation("io.mockk:mockk-android:1.13.8")
 
     // Compose (si lo usas)
-    implementation("com.google.android.material:material:1.12.0")
+    //implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
     implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.5.0-alpha04")
